4fd5cfa1b092ea3c733b8ab937948e04ca7517e8
Roxana-Munoz <135390411+Roxana-Munoz@users.noreply.github.com>
2025-05-24 16:25:53 -0300

    Add files via upload

---
+++ Categoria.php
@@ -0,0 +1,39 @@
+<?php
+//Creo la clase y sus atributos
+class Categoria{
+	private $idcategoria;
+	private $descripcion;
+	
+    //METODO CONSTRUCTOR
+	public function __construct($idcategoria, $descripcion ){
+		$this->idcategoria=$idcategoria;
+		$this->descripcion= $descripcion;
+	}
+    //METODOS GETERS
+    //Obtiene el valor de idCategoria
+    public function getidcategoria(){
+        return $this->idcategoria;
+    }
+    //Obtiene el valor de descripcion
+    public function getDescripcion(){
+        return $this->descripcion;
+    }
+
+    //METODOS SETERS
+   //Setea el valor de idCategoria
+    public function setidcategoria($idcategoria){
+        $this->idcategoria= $idcategoria;
+    }
+     //Setea el valor de descripcion
+    public function setDescripcion($descripcion){
+        $this->descripcion= $descripcion;
+    }
+
+    //METODO __TO STRING
+    public function __toString(){
+      
+        $cadena = "IdCategori: ".$this->getidcategoria()."\n";
+        $cadena = $cadena. "descripcion: ".$this->getDescripcion()."\n";
+    }
+}
+?>
